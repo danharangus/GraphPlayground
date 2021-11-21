6895f291b535741d5fdfc5ce97534de527c5ada5
group13-Harangus-Dan-Ioan <33331174+danharangus@users.noreply.github.com>
2021-11-21 23:34:36 +0200

    Added directed/undirected option + edge removal

--- ui/ui.py
+++ ui/ui.py
@@ -56,7 +56,7 @@ class UI:
             self.draw_node(node, screen, font)
         self.print_nodes()
 
-    def check_collision(self, point, rect):
+    def check_node_collision(self, point, rect):
         x1, y1, w, h = rect
         x2, y2 = x1 + w + 30, y1 + h + 30
         x1 -= 30
@@ -67,7 +67,40 @@ class UI:
                 return True
         return False
 
-    def draw_edge(self, edge, screen, font):
+    def draw_arrow(self, node1, node2, screen, color):
+        vx = float(node2.x - node1.x)
+        vy = float(node2.y - node1.y)
+
+        ux = vx / (math.sqrt(vx * vx + vy * vy))
+
+        uy = vy / (math.sqrt(vx * vx + vy * vy))
+        xf1 = float(node1.x + 40 * ux)
+        xf2 = float(node2.x - 40 * ux)
+        yf1 = float(node1.y + 40 * uy)
+        yf2 = float(node2.y - 40 * uy)
+
+        theta = math.pi / 6
+
+        rvx1 = float(-vx * math.cos(theta) + vy * math.sin(theta))
+
+        rvy1 = float(-vx * math.sin(theta) - vy * math.cos(theta))
+
+        rv1len = math.sqrt(rvx1 * rvx1 + rvy1 * rvy1)
+
+        rvx2 = float(-vx * math.cos(-theta) + vy * math.sin(-theta))
+        rvy2 = float(-vx * math.sin(-theta) - vy * math.cos(-theta))
+
+        rv2len = math.sqrt(rvx2 * rvx2 + rvy2 * rvy2)
+
+        p1x = float(xf2 + rvx1 / rv1len * 40)
+        p1y = float(yf2 + rvy1 / rv1len * 40)
+        p2x = float(xf2 + rvx2 / rv2len * 40)
+        p2y = float(yf2 + rvy2 / rv2len * 40)
+        pygame.draw.line(screen, color, (xf2, yf2), (p1x, p1y))
+        pygame.draw.line(screen, color, (xf2, yf2), (p2x, p2y))
+
+
+    def draw_edge(self, edge, screen):
         n1 = edge.x
         n2 = edge.y
         vx = (n1.x - n2.x)
@@ -79,26 +112,41 @@ class UI:
         yf1 = float(n1.y - 40 * uy)
         yf2 = float(n2.y + 40 * uy)
         color = edge.color
-        pygame.draw.line(screen, color,
+        rect = pygame.draw.line(screen, color,
                          (xf1, yf1),
                          (xf2, yf2), 2)
+        if edge.directed is True:
+            self.draw_arrow(edge.x, edge.y, screen, color)
+        for e in self.__graph.edges:
+            if e.x == edge.x and e.y == edge.y:
+                e.rect = rect
+                break
         pygame.display.update()
 
-    def draw_edges(self, screen, font):
+    def draw_edges(self, screen):
         for edge in self.__graph.edges:
-            self.draw_edge(edge, screen, font)
+            self.draw_edge(edge, screen)
         self.print_edges()
 
     def draw_graph(self, screen, font, canvas_rect):
         screen.fill(self.__BLACK, canvas_rect)
         self.draw_nodes(screen, font)
-        self.draw_edges(screen, font)
+        self.draw_edges(screen)
 
     def point_in_rect(self, point, rect):
         x1, y1, w, h = rect
         x2, y2 = x1 + w, y1 + h
         x, y = point
-        if x1 <  x < x2:
+        if x1 < x < x2:
+            if y1 < y < y2:
+                return True
+        return False
+
+    def check_edge_click(self, point, rect):
+        x1, y1, w, h = rect
+        x2, y2 = x1 + w, y1 + h
+        x, y = point
+        if x1 < x < x2:
             if y1 < y < y2:
                 return True
         return False
@@ -132,12 +180,15 @@ class UI:
         # draw canvas
         screen.blit(canvas, (0, 200))
 
-        button_text = font.render("Directed", True, self.__WHITE)
-        directed_button = pygame.Rect(w / 17, h / 18,  w / 10, h / 20)
-
+        button_message = "Directed"
+        is_directed = False
+        directed_button_position_w_ratio = 17
+        directed_button_position_h_ratio = 18
+        button_text = font.render(button_message, True, self.__WHITE)
+        directed_button = pygame.Rect(w / directed_button_position_w_ratio, h / directed_button_position_h_ratio,
+                                      w / 9, h / 20)
         pygame.draw.rect(screen, self.__BUTTON_NORMAL, directed_button)
-        screen.blit(button_text, (w / 17 + 25, h / 18))
-
+        screen.blit(button_text, (w / directed_button_position_w_ratio + 25, h / directed_button_position_h_ratio))
         pygame.display.update()
 
         counter = 0
@@ -146,7 +197,7 @@ class UI:
         destination_node = None
         while loop:
             pygame.draw.rect(screen, self.__BUTTON_NORMAL, directed_button)
-            screen.blit(button_text, (w / 17 + 25, h / 18))
+            screen.blit(button_text, (w / directed_button_position_w_ratio + 25, h / directed_button_position_h_ratio))
             pygame.display.flip()
             try:
                 for event in pygame.event.get():
@@ -156,27 +207,57 @@ class UI:
                     elif event.type == pygame.MOUSEBUTTONDOWN:
                         pos = self.get_pos()
 
-                        collision = False   # check collision
+                        node_collision = False   # check collision
                         for node in self.__graph.nodes:
                             if node.rect is not None:
-                                if self.check_collision(pos, node.rect):
-                                    collision = True
+                                if self.check_node_collision(pos, node.rect):
+                                    node_collision = True
                                     break
 
                         clicked_node = None
-                        click = False
+                        clicked_edge = None
+                        node_click = False
+                        edge_click = False
+
                         for node in self.__graph.nodes:
                             if node.rect is not None:
                                 if self.point_in_rect(pos, node.rect):
                                     clicked_node = node
-                                    click = True
+                                    node_click = True
                                     break
 
+                        if not node_click:
+                            for edge in self.__graph.edges:
+                                if edge.rect is not None:
+                                    if self.check_edge_click(pos, edge.rect):
+                                        edge_click = True
+                                        clicked_edge = edge
+                                        break
+
                         if event.button == 1:
-                            if w/17 <= float(pos[0]) <= w / 17 + w / 10 and h/ 18 <= float(pos[1]) <= h / 18 + h / 20:
-                                print("Button clicked")
+                            if w/17 <= float(pos[0]) <= w / directed_button_position_w_ratio + w / 10 and \
+                                    h / directed_button_position_h_ratio <= float(pos[1]) <= h / directed_button_position_h_ratio + h / 20:
+                                # Click on directed/undirected button
+                                if button_message == "Directed":
+                                    button_message = "Undirected"
+                                    is_directed = True
+                                    for edge in self.__graph.edges:
+                                        edge.directed = True
+                                else:
+                                    button_message = "Directed"
+                                    is_directed = False
+                                    for edge in self.__graph.edges:
+                                        edge.directed = False
+                                self.draw_graph(surface, font, canvas_rect)
+                                screen.blit(surface, (0, 0))
+
+                                button_text = font.render(button_message, True, self.__WHITE)
+                                directed_button = pygame.Rect(w / directed_button_position_w_ratio, h / directed_button_position_h_ratio, w / 9, h / 20)
+                                pygame.draw.rect(screen, self.__BUTTON_NORMAL, directed_button)
+                                screen.blit(button_text, (w / directed_button_position_w_ratio + 25, h / directed_button_position_h_ratio))
+                                pygame.display.update()
                                 continue
-                            if click:
+                            if node_click:
                                 if source_node is None:
                                     source_node = clicked_node
                                     source_node.color = self.__BLUE
@@ -185,7 +266,7 @@ class UI:
                                     pygame.display.flip()
                                 else:
                                     if source_node != clicked_node:
-                                        new_edge = Edge(source_node, clicked_node, self.__WHITE, False)
+                                        new_edge = Edge(source_node, clicked_node, self.__WHITE, is_directed)
                                         self.__graph.insert_edge(new_edge)
                                         self.draw_graph(surface, font, canvas_rect)
                                         screen.blit(surface, (0, 0))
@@ -196,9 +277,10 @@ class UI:
                                     pygame.display.flip()
                                     clicked_node = None
                                     source_node = None
-                            elif not collision:
+                            elif not node_collision:
                                 if source_node is None:
                                     if pos[1] > 235:
+                                        # Add new node
                                         new_node = Node(counter, pos[0], pos[1], self.__WHITE)
                                         self.__graph.insert_node(new_node)
                                         self.draw_graph(surface, font, canvas_rect)
@@ -213,9 +295,16 @@ class UI:
                                     clicked_node = None
                                     source_node = None
                         elif event.button == 3:
-                            if click:
+                            if node_click:
+                                # Remove node
                                 self.__graph.remove_node(clicked_node)
                                 clicked_node = None
+                                self.draw_graph(surface, font, canvas_rect)
+                                screen.blit(surface, (0, 0))
+                                pygame.display.flip()
+                            elif edge_click:
+                                self.__graph.remove_edge(clicked_edge)
+                                clicked_edge = None
                                 self.draw_graph(surface, font, canvas_rect)
                                 screen.blit(surface, (0, 0))
                                 pygame.display.flip()
